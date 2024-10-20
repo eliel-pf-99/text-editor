7b4bc5d6287023b0ad097f6e9bfc52698f243831
Eliel P. F <eliel.dev.99@gmail.com>
2024-10-19 23:27:38 -0300

    update api url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080"
+axios.defaults.baseURL = "https://text-editor-go.onrender.com"
 
 interface LoginRes {
   email: string
